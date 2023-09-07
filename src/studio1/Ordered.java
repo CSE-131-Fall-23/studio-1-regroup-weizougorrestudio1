60fe5d4eb58ae669bf4fabff0c7f25934df2285b
tanvigorre <tanvigorre@localhost>
2023-09-07 11:03:51 -0500

    Yay! We can code! We did some stuff!

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -9,12 +9,14 @@ public class Ordered {
 	 */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.println("Value for x?");
 		int x = in.nextInt();
-		System.out.println("Value for y?");
 		int y = in.nextInt();
-		System.out.println("Value for z?");
 		int z = in.nextInt();
+		boolean isOrdered = false;
+		if (x < y && y < z || z < y && y < x) {
+			isOrdered = true;
+		}
+		System.out.println(isOrdered);
 
 	}
 
